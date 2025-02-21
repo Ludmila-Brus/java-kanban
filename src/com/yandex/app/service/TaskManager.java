c8bd650a3318b4aae91077267ee513cbfe956456
Ludmila-Brus <luntalumi@yandex.ru>
2025-02-21 13:26:48 +0300

    210225_first_commit

--- src/com/yandex/app/service/TaskManager.java
+++ src/com/yandex/app/service/TaskManager.java
@@ -3,7 +3,6 @@ package com.yandex.app.service;
 import com.yandex.app.model.Epic;
 import com.yandex.app.model.SubTask;
 import com.yandex.app.model.Task;
-
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.TreeSet;
